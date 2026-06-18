f1dbbf63de4a1a557c36d61e06224dad2bfe62f1
Vishvananda Abrams <vishvananda@gmail.com>
2026-06-18 07:49:39 -0700

    Improve focused ref-test handling

--- scripts/pa_run_check_targets.mk
+++ scripts/pa_run_check_targets.mk
@@ -6,6 +6,9 @@ RUNNER ?= $(if $(strip $(CPPGM_TEST_APP)),$(CPPGM_TEST_APP),../dev/$(RUN_LABEL))
 TARGET_ARGS ?=
 CHECK_RUN ?= scripts/run_all_tests.pl $(RUNNER) check "$(TEST)"
 CHECK_COMPARE ?= scripts/compare_results.pl ref check "$(TEST)"
+REF_TEST_ROOT ?= tests
+REF_TEST_INPUT = $(if $(strip $(TEST)),$(TEST),$(REF_TEST_ROOT))
+REF_TEST_RUN ?= scripts/run_all_tests.pl $(REF_TEST_APP) ref "$(REF_TEST_INPUT)"
 
 run: $(RUN_CHECK_DEPS)
 	@if [ -z "$(INPUT)" ]; then \
--- scripts/run_cpptoolchain_tests_worker.pl
+++ scripts/run_cpptoolchain_tests_worker.pl
@@ -15,6 +15,7 @@ use CppgmBatchWorker qw(
 	clear_progress_state
 	close_worker
 	collect_tests
+	ensure_test_app_available
 	get_timeout_from_env
 	note_progress_state
 	open_worker
@@ -350,6 +351,7 @@ sub run_toolchain_tests
 }
 
 my ($app, $suffix, $tests_root) = @ARGV;
+ensure_test_app_available($app, $suffix, $tests_root);
 my @tests = collect_tests($tests_root, qr/\.t$/);
 my $verbose = $ENV{VERBOSE} || $ENV{CPGM_TEST_VERBOSE};
 my $keep_going = $ENV{KEEP_GOING};
